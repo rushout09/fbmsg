aec4fd67b8741808b2b0e972f3e0690b0eed06fc
rushout09 <rushabh.agarwal9@gmail.com>
2017-11-04 02:42:10 +0530

    Minor mistake in a function

--- index.js
+++ index.js
@@ -60,4 +60,6 @@ function handleMessage(sender_psid, received_message){
 function handlePostback(sender_psid, received_postback){
 }
 //Sends response messages via the Send API
-function callSendAPI(sender_psid, response
+function callSendAPI(sender_psid, response)
+{
+}
