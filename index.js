68e1938b1a48f4af18256b6fda8d2ee3abb6c874
Rushabh Agarwal <rushabh.agarwal9@gmail.com>
2017-11-07 20:28:56 +0530

    New token

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR VERIFY TOKEN>";
+  const VERIFY_TOKEN = "someshit";
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
